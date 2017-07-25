2fcaebd928560ee94986fcc387b740f20f4cbbdf
venkatesh Pittala <venkirowling@gmail.com>
2017-07-25 18:03:53 -0400

    " added a for loop "

--- Sprint118/src/devops/sprint118/epicsystems/DevopsEng.java
+++ Sprint118/src/devops/sprint118/epicsystems/DevopsEng.java
@@ -1,11 +1,15 @@
 package devops.sprint118.epicsystems;
 
 public class DevopsEng {
-
+ 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     System.out.println("DEVOPS Engineer 1");
     System.out.println("Dev Engineer 3");
-	}
-
+     for(int i=0;i<11;i++)
+     {
+    System.out.println("count is :" + i);
+     }
+     }
 }
+
